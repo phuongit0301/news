dd6aa4a1d9b95d0c369d8ed2881c10a4d12d0bdc
phuongpt <phuongit0301@mytrax.co.jp>
2015-12-22 17:31:45 +0700

    Update code

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,32 +11,33 @@
 |
 */
 
-
 Route::resource('/', 'IndexController');
 Route::get('tin-tuc/{slug}', 'CategoryController@index');
+
 //Route::get('categories/{slugCategory}/{slugPost}', 'PostController@index');
 
-Route::controllers([
-    'auth' => 'Auth\AuthController',
-    'password' => 'Auth\PasswordController',
-]);
-
-
+Route::controllers(['auth' => 'Auth\AuthController', 'password' => 'Auth\PasswordController', ]);
 
 Route::get('/dashboard', 'Auth\UserController@index');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
-
-    Route::get('/', [ 'as' => 'admin.layout.index', 'uses' => 'Admin\AdminController@index']);
-
-    Route::resource('movies',  'Movies\MoviesController');
-
+    
+    Route::get('/', ['as' => 'admin.layout.index', 'uses' => 'Admin\AdminController@index']);
+    
+    Route::resource('movies', 'Movies\MoviesController');
+    
     Route::resource('categories', 'Admin\CategoryController');
-
+    
     Route::resource('posts', 'Admin\PostController');
-
+    
     Route::post('files/upload', ['uses' => 'Admin\FileController@fileUpload', 'as' => 'files.upload']);
     Route::get('files/list', ['uses' => 'Admin\FileController@fileList', 'as' => 'files.list']);
-
+    
     Route::get('/upload', ['uses' => 'Admin\UploadController@index', 'as' => 'admin.upload']);
+    
+    // Add the following routes
+    Route::post('upload/file', 'UploadController@uploadFile');
+    Route::delete('upload/file', 'UploadController@deleteFile');
+    Route::post('upload/folder', 'UploadController@createFolder');
+    Route::delete('upload/folder', 'UploadController@deleteFolder');
 });
--- app/Library/UploadsManager.php
+++ app/Library/UploadsManager.php
@@ -11,7 +11,7 @@ class UploadsManager
     protected $mimeDetect;
     
     public function __construct(PhpRepository $mimeDetect) {
-        $this->disk = Storage::disk(config('blog.uploads.storage'));
+        $this->disk = Storage::disk(config('files.uploads.storage'));
         $this->mimeDetect = $mimeDetect;
     }
     
@@ -89,7 +89,7 @@ class UploadsManager
      * Return the full web path to a file
      */
     public function fileWebpath($path) {
-        $path = rtrim(config('blog.uploads.webpath'), '/') . '/' . ltrim($path, '/');
+        $path = rtrim(config('files.uploads.webpath'), '/') . '/' . ltrim($path, '/');
         return url($path);
     }
     
@@ -113,4 +113,61 @@ class UploadsManager
     public function fileModified($path) {
         return Carbon::createFromTimestamp($this->disk->lastModified($path));
     }
+
+    public function createDirectory($folder)
+  {
+    $folder = $this->cleanFolder($folder);
+
+    if ($this->disk->exists($folder)) {
+      return "Folder '$folder' aleady exists.";
+    }
+
+    return $this->disk->makeDirectory($folder);
+  }
+
+  /**
+   * Delete a directory
+   */
+  public function deleteDirectory($folder)
+  {
+    $folder = $this->cleanFolder($folder);
+
+    $filesFolders = array_merge(
+      $this->disk->directories($folder),
+      $this->disk->files($folder)
+    );
+    if (! empty($filesFolders)) {
+      return "Directory must be empty to delete it.";
+    }
+
+    return $this->disk->deleteDirectory($folder);
+  }
+
+  /**
+   * Delete a file
+   */
+  public function deleteFile($path)
+  {
+    $path = $this->cleanFolder($path);
+
+    if (! $this->disk->exists($path)) {
+      return "File does not exist.";
+    }
+
+    return $this->disk->delete($path);
+  }
+
+  /**
+   * Save a file
+   */
+  public function saveFile($path, $content)
+  {
+    $path = $this->cleanFolder($path);
+
+    if ($this->disk->exists($path)) {
+      return "File already exists.";
+    }
+
+    return $this->disk->put($path, $content);
+  }
 }
